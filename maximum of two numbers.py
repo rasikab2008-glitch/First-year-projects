2eb7f7aeae96ea2e0ab5d44bb4d1d417a31ab7a7
rasikab2008-glitch <rasikab2008@gmail.com>
2026-07-09 19:30:51 +0530

    Add files via upload
    Upload maximum of two numbers

---
+++ maximum of two numbers.py
@@ -0,0 +1,9 @@
+def largest(a,b):
+   if(a>b):
+       print("largest number is",a)
+   else:
+       print("largest number is",b)
+c=int(input("enter num1:"))
+d=int(input("enter num2:"))
+largest(c,d)
+    

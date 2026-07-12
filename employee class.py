b093bfba198f9904a969c5d8363115eea4747335
rasikab2008-glitch <rasikab2008@gmail.com>
2026-07-12 21:13:26 +0530

    Add files via upload
    Upload employee class

---
+++ employee class.py
@@ -0,0 +1,10 @@
+class employee():
+    def __init__(self,name,salary):
+        self.name=name
+        self.salary=salary
+    def display(self):
+        print(self.name)
+        print(self.salary)
+emp1=employee("rasi",90000)
+emp1.display()
+        

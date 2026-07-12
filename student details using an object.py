ffb6d3a497af3676382bcca16b787b96a866adb1
rasikab2008-glitch <rasikab2008@gmail.com>
2026-07-12 21:14:25 +0530

    Add files via upload
    Upload student details using an object

---
+++ student details using an object.py
@@ -0,0 +1,17 @@
+class student():
+    def __init__(self,name,age,course):
+        self.name=name
+        self.age=age
+        self.course=course
+
+    def display(self):
+        print("name:",self.name)
+        print("age:",self.age)
+        print("course:",self.course)
+        print("enter 
+
+std1=student("rasika","17","bsc aiml")
+std2=student("ganapathy","20","btech it")
+std1.display()
+std2.display()
+        

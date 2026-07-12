4b39e8ec67d1b04e80236cfcc1f7827568269228
rasikab2008-glitch <rasikab2008@gmail.com>
2026-07-12 21:10:05 +0530

    Add files via upload
    Upload class variable

---
+++ use class variable in a program.py
@@ -0,0 +1,12 @@
+class student:
+    college="karpagam"
+    def __init__(self,name):
+     self.name=name
+s1=student("ganapathy")
+s2=student("ronaldo")
+print("student1:",s1.name,"-",s1.college)
+print("student2:",s2.name,"-",s2.college)
+
+
+    
+    

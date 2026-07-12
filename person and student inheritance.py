52a505a55a8450d77fec61e96d31aeb84cced8ea
rasikab2008-glitch <rasikab2008@gmail.com>
2026-07-12 21:12:58 +0530

    Add files via upload
    Upload person and student inheritance

---
+++ person and student inheritance.py
@@ -0,0 +1,9 @@
+class person:
+    def display_name(self):
+        print("name:rasika")
+class student(person):
+    def display_course(self):
+        print("course:bsc aiml")
+std=student()
+std.display_name()
+std.display_course()

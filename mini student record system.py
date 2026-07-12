fef5def2ad9f9ff11373b6c1e7ce45c05b7179a8
rasikab2008-glitch <rasikab2008@gmail.com>
2026-07-12 21:10:56 +0530

    Add files via upload
    Upload mini student record system

---
+++ mini student record system.py
@@ -0,0 +1,8 @@
+print("_ _ _ _ _student record _ _ _ _ _")
+student_name=(input("Enter the student name:"))
+roll_no=int(input("Enter the roll no:"))
+mark=int(input("Enter the mark:"))
+if(mark>=35):
+    print("PASS")
+else:
+    print("FAIL")
